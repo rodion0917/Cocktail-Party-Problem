c8c626711758f50a4028e59b16a2d6edbef88238
Ayush Mishra <88069296+rodion0917@users.noreply.github.com>
2022-05-30 14:58:21 +0530

    Update preprocess_sound.py

--- sounds/preprocess_sound.py
+++ sounds/preprocess_sound.py
@@ -4,8 +4,8 @@ from scipy.io import wavfile
 import utilities as utl
 
 # Read the .wav files as numpy arrays
-rate1, data1 = wavfile.read("remember_me1.wav")
-rate2, data2 = wavfile.read("remember_me2.wav")
+rate1, data1 = wavfile.read("sourceX.wav")
+rate2, data2 = wavfile.read("sourceY.wav")
 
 # Plot the sounds as time series data
 utl.plotSounds([data1, data2], ["Instruments", "Vocals"], rate1, "../plots/sounds/Remember_me")
@@ -18,5 +18,5 @@ data1 = data1[0:minimum]
 data2 = data2[0:minimum]
 
 # writing the array into to the wav file with sampling rate which is average of the two
-wavfile.write("remember_me1.wav", (rate1 + rate2)/2, data1)
-wavfile.write("remember_me2.wav", (rate1 + rate2)/2, data2)
+wavfile.write("sourceX.wav", (rate1 + rate2)/2, data1)
+wavfile.write("sourceY.wav", (rate1 + rate2)/2, data2)
